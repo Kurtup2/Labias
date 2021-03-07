e67d67f4bfd7bb166264ccf01ac0e2a33bae44de
RudolfsZ <ruudis2004@inbox.lv>
2021-03-07 19:42:33 +0000

    Pamats

--- main.cpp
+++ main.cpp
@@ -1,7 +1,40 @@
 #include <iostream>
+#include <cmath>
+#include <string>
+
+using namespace std;
+void myFunction ( int lino, int rullis, int plat, int gar){
+
+  int sum, summa1 ; 
+  sum = lino * plat;
+  cout << " Izmaksas izklājot platumā :\n" <<sum;
+
+  int summ, summa2 ;
+  summ = lino * gar;
+  cout << " Izmaksas izklājot garumā :\n" <<summ;
+
+  int summm, summa3 ;
+  summm = lino * ( plat * gar );
+  cout << " Izmaksas izklājot pa visu telpu :\n" <<summm;
+}
+
 
 int main() {
-  std::cout << "Hello World!\n";
-  int re ;
-}// es te 
-// tu te 
+int lino, sum ;
+cout << "Ievadi cik maksā linolejs EUR/m² :\n";
+cin >> lino;
+
+int rullis;
+cout << " Ievadi linoleja ruļļa platumu metros :\n";
+cin >> rullis;
+
+int plat;
+cout << " Ievadi telpas platumu metros :\n";
+cin >> plat;
+
+int gar;
+cout << " Ievadi telpas garumu metros :\n";
+cin >> gar;
+
+myFunction ( lino, rullis, plat, gar);
+} 
